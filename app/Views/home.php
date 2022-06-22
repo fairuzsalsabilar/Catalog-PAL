503e069edc81bbe2d95a3d1ce1069720b0f2d1c7
iruz <fairuzsalsabila@student.ub.ac.id>
2022-06-22 14:52:59 +0700

    nambah style dan navbar

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+    </style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>CATALOG PT. PAL INDONESIA</title>
-
 </head>
 <body>
-    
+    <!---Navbar--->
+    <ul class="navbar">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+        </li>
+    </ul>
 </body>
 </html>
