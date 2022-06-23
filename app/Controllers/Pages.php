dbecf9f10dc1263c99d365047010ed85e6310e56
Ayu Puspita R <ayu.puspita747@gmail.com>
2022-06-23 19:22:28 +0700

    baru egen

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,9 +6,7 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('layout/header');
         echo view('pages/home');
-        echo view('layout/footer');
     }
 
     public function home()
