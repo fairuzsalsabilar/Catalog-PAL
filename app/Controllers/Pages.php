ef5956bbc0701311eea93b491c38d8e2af6a568d
Ayu Puspita R <ayu.puspita747@gmail.com>
2022-06-27 15:40:28 +0700

    home progress

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,7 +6,7 @@ class Pages extends BaseController
 {
     public function index()
     {
-        echo view('pages/login');
+        echo view('pages/home');
     }
 
     public function home()
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Catalog PT. PAL Indonesia</title>
+    <title>Hello, world!</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
@@ -30,7 +30,74 @@
 
     <!-- Home atas -->
     <div class="imgMain">
-      <img src="img/foto-home.jpg" width="auto" height="577px">
+      <img src="img/foto-home.jpg" width="871" height="577px">
+    </div>
+
+    <!-- Btn Search --> 
+    <div class="btn-cari">
+      <button type="button" class="btn-search btn-primary btn-lg scrollto">
+        <img src="img/search.png" class="search" width="26px" height="auto">
+      </button>
+    </div>
+
+    <!-- informasi -->
+    <div class="kotak">
+      <h6>INFORMASI</h6>
+      <a class="tulisan">Katalog Berkas<br>PT.PAL Indonesia</a>
+    </div>
+
+    <!-- bundaran -->
+    <div class="garis"></div>
+    <div class="bundaran">
+      <div class="bundarkiri"></div>
+      <div class="bundarkiri1"></div>
+      <div class="bundartengah">
+        <a class="infoJumlah">Jumlah</a>
+        <a class="jumlah">Berkas</a>
+      </div>
+      <div class="bundarkanan"></div>
+      <div class="bundarkanan1"></div>
+    </div>
+    <div class="garis1"></div>
+
+    <div class="kotakbawah"></div>
+    <div class="prosedur">
+      <div class="container">
+        <div class="heading">
+           <h2>Prosedur Pengguna</h2>
+        </div>
+        <div class="langkahh">
+          <div class="langkah">
+            <div class="langkah-icon">
+              <img src="img/icon-web.png">
+              <p>Website</p>
+            </div>
+            <p class="txt-contents">Membuka website terlebih dahulu</p>
+          </div>
+          <div class="langkah">
+            <div class="langkah-icon">
+              <img src="img/icon-cari.png">
+              <p>Pencarian</p>
+            </div>
+            <p class="txt-contents">Masuk ke bagian kolom pencarian</p>
+          </div>
+          <div class="langkah">
+            <div class="langkah-icon">
+              <img src="img/icon-input.png">
+              <p>Input</p>
+            </div>
+            <p class="txt-contents">Masukkan nama berkas yang ingin dicari</p>
+          </div>
+          <div class="langkah">
+            <div class="langkah-icon">
+              <img src="img/icon-output.png">
+              <p>Output</p>
+            </div>
+            <p class="txt-contents">Berkas yang dicari akan ditampilkan</p>
+          </div>
+          <div class="clear"></div>
+        </div>
+      </div>
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
