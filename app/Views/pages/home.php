dff77e0f3d2aed3bf99a13094189ac6931be5979
Ayu Puspita R <ayu.puspita747@gmail.com>
2022-06-30 15:20:24 +0700

    home

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -15,89 +15,147 @@
   </head>
 
   <body>
-
-    <header id="header" class="fixed-top">
-      <div class="header-left">
-          <a class="logo mr-auto" href="#">
-            <img src="img/logo PAL.png" width="300" height="29.7" alt="">
-          </a>
+ 
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand navbar" href="#">
+          <img src="img/logo PAL.png" width="300px" height="29.7px">
+        </a>
+        <a class="btn btn-primary tombol btn-lg" href="#" role="button">Log In</a>
       </div>
-    <!-- Btn Login -->
-      <div class="header-right">  
-        <button type="button" class="btn-login btn-primary btn-lg scrollto">Log in</button>
-      </div>
-    </header>
+    </nav>
+    <!-- Akhir Navbar -->
 
     <!-- Home atas -->
-    <div class="imgMain">
-      <img src="img/foto-home.jpg" width="871" height="577px">
-    </div>
-
-    <!-- Btn Search --> 
-    <div class="btn-cari">
-      <button type="button" class="btn-search btn-primary btn-lg scrollto">
-        <img src="img/search.png" class="search" width="26px" height="auto">
-      </button>
-    </div>
-
-    <!-- informasi -->
-    <div class="kotak">
-      <h6>INFORMASI</h6>
-      <a class="tulisan">Katalog Berkas<br>PT.PAL Indonesia</a>
-    </div>
-
-    <!-- bundaran -->
-    <div class="garis"></div>
-    <div class="bundaran">
-      <div class="bundarkiri"></div>
-      <div class="bundarkiri1"></div>
-      <div class="bundartengah">
-        <a class="infoJumlah">Jumlah</a>
-        <a class="jumlah">Berkas</a>
-      </div>
-      <div class="bundarkanan"></div>
-      <div class="bundarkanan1"></div>
-    </div>
-    <div class="garis1"></div>
-
-    <div class="kotakbawah"></div>
-    <div class="prosedur">
-      <div class="container">
-        <div class="heading">
-           <h2>Prosedur Pengguna</h2>
+    <div class="homeAtas">
+    <div class="container-fluid home">
+      <!-- Gambar -->
+      <div class="col-4">
+          <div class="kotak">
+          <h6>INFORMASI</h6>
+          <a class="tulisan">Katalog Berkas<br>PT.PAL Indonesia</a>
         </div>
-        <div class="langkahh">
-          <div class="langkah">
-            <div class="langkah-icon">
-              <img src="img/icon-web.png">
-              <p>Website</p>
-            </div>
-            <p class="txt-contents">Membuka website terlebih dahulu</p>
-          </div>
-          <div class="langkah">
-            <div class="langkah-icon">
-              <img src="img/icon-cari.png">
-              <p>Pencarian</p>
-            </div>
-            <p class="txt-contents">Masuk ke bagian kolom pencarian</p>
-          </div>
-          <div class="langkah">
-            <div class="langkah-icon">
-              <img src="img/icon-input.png">
-              <p>Input</p>
-            </div>
-            <p class="txt-contents">Masukkan nama berkas yang ingin dicari</p>
-          </div>
-          <div class="langkah">
-            <div class="langkah-icon">
-              <img src="img/icon-output.png">
-              <p>Output</p>
-            </div>
-            <p class="txt-contents">Berkas yang dicari akan ditampilkan</p>
-          </div>
-          <div class="clear"></div>
+        </div>
+      <div class="row justify-content-end">
+        <!-- informasi -->
+        <div class="col-7">
+          <img src="img/foto-home.jpg" class="img-fluid imgMain" width="871" height="577px">
+        <div class="kotakElemen"></div>
+        </div>
+  
+      <!-- Btn Search --> 
+        <div class="btn-cari">
+          <button type="button" class="btn-search btn-primary btn-lg scrollto">
+            <img src="img/search.png" class="search" width="26px" height="auto">
+          </button>
         </div>
       </div>
+      </div>
+    </div>
+
+    <!-- Informasi Jumlah Berkas  -->
+    <div class="container data">
+        <div class="elemen el1"></div>
+        <div class="elemen el2"></div>
+        <div class="elemen el3"></div>
+        <div class="elemen el4">
+            <div class="deskripsi">
+                <h1>1.045</h1>
+                <p>Data</p>
+            </div>
+        </div>
+        <div class="elemen el3"></div>
+        <div class="elemen el2"></div>
+        <div class="elemen el1"></div>
+    </div>
+
+    <!-- Pencarian -->
+    .<div class="container-fluid pencarian">
+      <div class="row kotakcari">
+      <div class="col-6 kiri">
+        <img src="img/img cari.png" width="552.76px" height="457px">
+      </div>
+      <div class="col-6 kanan">
+      <div class="penjelasan">
+          <h6>PENCARIAN</h6>
+          <a>Cari Berkas<br>Kebutuhan Anda</a>
+          <p>Total berkas daskdlasjdklasdlkmaskdlmaskldmasklc dmkfnksdjfnksmd f</p>
+        </div>
+        <div class="kolomCari">
+          <div class="pilihanKategori">  
+              <a class="btn btn-outline-secondary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Label</a>
+              <button class="btn btn-outline-secondary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Judul Dokumen</button>
+              <a class="btn btn-outline-secondary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample3">Judul Proyek</a>
+              <button class="btn btn-outline-secondary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample4">Tahun</button>
+          </div>
+          <div class="carii">
+            <input class="form-control me-2 cari" type="search" placeholder="Ketik pencarian di sini" aria-label="Search">
+            <button type="button" class="btn btn-danger btn-src">Cari</button>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+    </div>
+
+    <!-- Prosedur Pengguna -->
+    <div class="container tlsnprosedur">
+      <p>Prosedur Pengguna</p>
+    </div>
+    <div class="kotakBwh">
+        <div class="dasar-warna">
+        </div>
+        <div class="container">
+        <div class="prosedur">
+            <div class="atas">
+            </div>
+            <div class="logo">
+                <img src="img/icon-web.png" alt="">
+                <h3>WEBSITE</h3>
+                <p>Membuka website terlebih<br>dahulu</p>
+            </div>
+            <div class="angka">
+                <p>1</p>
+            </div>
+        </div>
+        <div class="prosedur">
+            <div class="atas">
+            </div>
+            <div class="logo">
+                <img src="img/icon-cari.png" height="75px" alt="">
+                <h3> PENCARIAN</h3>
+                <p>Masuk ke bagian kolom<br>pencarian</p>
+            </div>
+            <div class="angka">
+                <p>2</p>
+            </div>
+        </div>
+        <div class="prosedur">
+            <div class="atas">
+            </div>
+            <div class="logo">
+                <img src="img/icon-input.png" height="75px" alt="">
+                <h3> INPUT</h3>
+                <p>Masukkan nama berkas yang<br>ingin dicari</p>
+            </div>
+            <div class="angka">
+                <p>3</p>
+            </div>
+        </div>
+        <div class="prosedur">
+            <div class="atas">
+            </div>
+            <div class="logo">
+                <img src="img/icon-output.png" alt="">
+                <h3> OUTPUT</h3>
+                <p>Berkas yang dicari akan<br>ditampilkan</p>
+            </div>
+            <div class="angka">
+                <p>4</p>
+            </div>
+        </div>
+        </div>
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
@@ -113,4 +171,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     -->
   </body>
+  <footer>
+    <p>Copyright © PT PAL 2022 Powered By SI UB 19</p>
+</footer>
 </html>
