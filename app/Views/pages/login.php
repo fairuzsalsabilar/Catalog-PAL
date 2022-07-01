24c669c25fcf9704f384b4e889110201c5c2e9d2
Devina Andhita Jaya Soegiarto <devinaandhita30@student.ub.ac.id>
2022-07-01 08:45:44 +0700

    login

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -126,8 +126,37 @@
       </div>
     </div>
     
-    <!-- End Example Code -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script> 
+    <!-- <section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Masukkan Username dan Password yang sudah terdaftar!</p>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typeusernameX">Username</label>
+                <input type="username" id="typeusernameX" class="form-control form-control-lg" />
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <label class="form-label" for="typePasswordX">Password</label>
+                <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+              </div>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> -->
   </body>
 </html>
